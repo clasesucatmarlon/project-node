2f28f2ba2bc99c4599fe5dd8001acb0417cae84d
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-23 08:55:49 -0500

    Proj-BE:FEAT 4 Created product controller

--- src/products/index.js
+++ src/products/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { ProductsController } = require('./controller');
 
 module.exports.ProductsAPI = (app) => {
     router
-        .get('/', (req, res) => {})    // http://localhost:3000/api/products/
-        .get('/:id', (req, res) => {}) // http://localhost:3000/api/products/23
-        .post('/', (req, res) => {})   // http://localhost:3000/api/products/
+        .get('/', ProductsController.getProducts)     // http://localhost:3000/api/products/
+        .get('/:id', ProductsController.getProduct)   // http://localhost:3000/api/products/23
+        .post('/', ProductsController.createProduct)  // http://localhost:3000/api/products/
 
     app.use('/api/products', router);
 };
