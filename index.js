b76e395191d2721c87241b9152188cf877dde762
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-23 08:36:04 -0500

    Proj-BE:FEAT 3 Created products module - routes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const debug = require('debug')('app:server');
 const { Config } = require('./src/config/index');
+const { ProductsAPI } = require('./src/products/index');
 
 const app = express();
 app.use(express.json());
 
 // Modules
-
+ProductsAPI(app);
 
 app.listen(Config.port, () => {
     debug(`Server running in port ${Config.port}`);
---
+++ src/products/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports.ProductsAPI = (app) => {
+    router
+        .get('/', (req, res) => {})    // http://localhost:3000/api/products/
+        .get('/:id', (req, res) => {}) // http://localhost:3000/api/products/23
+        .post('/', (req, res) => {})   // http://localhost:3000/api/products/
+
+    app.use('/api/products', router);
+};
