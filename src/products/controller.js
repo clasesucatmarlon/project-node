2501f59050c003d0f27376fec759b13aaab286bc
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-23 10:23:06 -0500

    Proj-BE:FEAT 5 Created product services

--- src/products/controller.js
+++ src/products/controller.js
@@ -1,7 +1,38 @@
 // Funciones controladoras de los endpoints
 
+const { ProductsService } = require('./services');
+const debug = require('debug')('app:module-products-controler')
+
 module.exports.ProductsController = {
-    getProducts: (req, res) => {},
-    getProduct: (req, res) => {},
-    createProduct: (req, res) => {},
+    getProducts: async (req, res) => {
+        try {
+            let products = await ProductsService.getAll();
+            res.json(products);
+        } catch (error) {
+            debug(error);
+            res.status(500).json({ message: 'Internal server error...' });
+        }
+    },
+    getProduct: async (req, res) => {
+        try {
+            const { params : { id }} = req;
+            const product = await ProductsService.getById(id);
+            res.json(product);
+            debug('Product found...');
+        } catch (error) {
+            debug(error);
+            res.status(500).json({ message: 'Internal server error...' });
+        }
+    },
+    createProduct: async (req, res) => {
+        try {
+            const { body } = req;
+            const insertedId = await ProductsService.createProd(body);
+            res.json(insertedId);
+            debug('Product inserted success..');
+        } catch (error) {
+            debug(error);
+            res.status(500).json({ message: 'Internal server error...' });
+        }
+    },
 };
