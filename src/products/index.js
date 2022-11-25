be9fa9c8f10a1efee02416dcc03a1c12bbdf65c6
Marlon GARCIA <clasesucatmarlon@gmail.com>
2022-11-25 10:49:34 -0500

    Proj-BE:FEAT 7 Created funcionality to delete prod

--- src/products/index.js
+++ src/products/index.js
@@ -6,6 +6,7 @@ module.exports.ProductsAPI = (app) => {
     router
         .get('/', ProductsController.getProducts)     // http://localhost:3000/api/products/
         .get('/:id', ProductsController.getProduct)   // http://localhost:3000/api/products/23
+        .delete('/:id', ProductsController.deleteProduct)   // http://localhost:3000/api/products/23
         .post('/', ProductsController.createProduct);  // http://localhost:3000/api/products/
 
     app.use('/api/products', router);
